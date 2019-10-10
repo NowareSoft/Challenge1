337e3932764b702c5dbb5c0f9d9248a684394834
Noware Software <Noware19@hotmail.com>
2019-10-10 14:53:08 -0300

    Proyecto terminado

---
+++ Chall1/Chall1.XF/Chall1.XF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Licensing;
+using Xamarin.Forms;
+
+namespace Chall1.XF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            SyncfusionLicenseProvider.RegisterLicense("MTQ3NzYxQDMxMzcyZTMyMmUzMEJJaW02UEVxS3dDQXg4Q2tEM0lYeGNrd2NpVm1ZODNzY21LN2h0bkY5OUk9");
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
